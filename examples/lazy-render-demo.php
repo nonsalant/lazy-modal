3bde16f5da7140dc850f732549d1002ddb0f1d03
Stefan Matei <nonsalant@gmail.com>
2025-07-12 14:08:17 +0300

    use an abortController to remove all listeners from triggers at once

--- examples/lazy-render-demo.php
+++ examples/lazy-render-demo.php
@@ -26,7 +26,7 @@
     <!-- storage form demo from: https://github.com/daviddarnes/storage-form -->
     <lazy-modal popover
         triggers=".storage-trigger"
-        close-button  in-head
+        close-button
         inner-scripts="https://www.unpkg.com/@daviddarnes/storage-form@2.0.1/storage-form.js"
     >
         <template>
@@ -37,7 +37,7 @@
     <!-- action table demo from: https://github.com/colinaut/action-table -->
     <lazy-modal popover
         triggers=".table-trigger"
-        close-button in-head
+        close-button
         inner-styles="https://unpkg.com/@colinaut/action-table/dist/action-table.css" 
         inner-scripts="https://unpkg.com/@colinaut/action-table/dist/index.js, 
             https://unpkg.com/@colinaut/action-table/dist/action-table-switch.js"
