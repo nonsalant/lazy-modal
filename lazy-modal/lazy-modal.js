e51447e53be7b053e57f6553814a363c37360387
Stefan Matei <nonsalant@gmail.com>
2026-01-29 11:23:25 +0200

    todo comment in trigger behavior setup
    for 'load' in #setupTriggerBehavior: added a comment questioning if an early return is needed after loading assets

--- lazy-modal/lazy-modal.js
+++ lazy-modal/lazy-modal.js
@@ -69,7 +69,10 @@ export default class LazyModal extends Base {
     }
 
     #setupTriggerBehavior() {
-        if (this.#loadOn === 'load') this.loadAssets(); // Load assets immediately if 'load' is set
+        if (this.#loadOn === 'load') {
+            this.loadAssets(); // Load assets immediately if 'load' is set
+            // to do: return here?
+        }
 
         if (!this.#triggers.length) return console.warn('LazyModal: No trigger element found');
 
